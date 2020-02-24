a72f013eda51cff5f651e3437d2f4fc4d714c934
lugovczov <vova.lugovczov@mail.ru>
2020-02-24 20:09:05 +0300

    feat: add solution func min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+    
+    if (array === undefined || array.length == 0 ) {
+      return 0;
+    }
+
+    array.sort((a,b) => a - b );
+    
+    return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+
+    if (array === undefined || array.length == 0 ) {
+      return 0;
+    }
+
+    array.sort((a,b) => a - b);
+    
+    return array[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+    if (array === undefined || array.length == 0 ) {
+      return 0;
+    }
+
+    let res = array.reduce((a,b) => a + b);
+    let res1 = (res / (array.length )).toFixed(2);
+    
+    return res1;
 }
